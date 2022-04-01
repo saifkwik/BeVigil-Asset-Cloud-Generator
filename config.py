06fb782c171322a8f4c5b5a668c3b9d7f84aae55
Saif Ahmad <saifkwik@gmail.com>
2022-04-02 01:25:27 +0530

    no changes

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import sys
 # Enter your API Key below:
 api_key = '<Enter your API Key here>'
 if f'<' in api_key:
-    print('INVALID API Key - Insert your Key in config.py file')
+    print('INVALID API Key - Insert  Key in config.py file')
     sys.exit()
